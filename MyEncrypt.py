df7c817808272bf41bbcd7305dcc66379c5ab554
John Vergara <john.vergara01@gmail.com>
2018-10-11 18:50:20 -0700

    commit

--- MyEncrypt.py
+++ MyEncrypt.py
@@ -1,30 +1,92 @@
 import os
 
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.ciphers import(Cipher, algorithms,modes)
 
+def MyEncrypt(message, key):
+	backend = default_backend()
+	IV = os.urandom(16)
+	cipher = Cipher(algorithms.AES(key), modes.CBC(IV), backend=backend)
+	encryptor = cipher.encryptor()
+	buf = bytearray(len(message) + 127)
+	len_encrypted = encryptor.update_into(message, buf)
+	C = bytes(buf[:len_encrypted]) + encryptor.finalize()
+	return (C, IV)
 
-
-def Myencrypt(message,key):
-	IV = os.random(2)
-	encryptor = Cipher(algorithms.AES(key),modes.GCM(IV),backend=default_backend()).encryptor()
-	C = encryptor.update(message) + encryptor.finalize()
-	
-	return (C,IV)
-
-def MyfileEncrypt(filepath):
-	key = os.random(4)
-	
-	f= open(filepath,'r+')
+def MyFileEncrypt(filepath):
+	key = os.urandom(32)
+	f = open(filepath, 'r')
+	ext = os.path.splitext(filepath)[1]
 	message = f.read()
-	ext = os.path.splitext(filepath)
-	f=close()
-	enc = Myencrypt(message,key)
-	
-	return(C,IV,key,ext)
+	message = bytes(message.encode('utf8'))
+	cipher = MyEncrypt(message, key)
+	C = cipher[0]
+	IV = cipher[1]
+	return (C, IV, key, ext)
 
-def Mydecrypt(C,IV,key,ext):
-	
-	
-	
+def MyDecrypt(C, IV, key, ext):
+	backend = default_backend()
+	cipher = Cipher(algorithms.AES(key), modes.CBC(IV), backend=backend)
+	decryptor = cipher.decryptor()
+	buf = bytearray(len(C) + 127)
+	len_decrypted = decryptor.update_into(C, buf)
+	output = bytes(buf[:len_decrypted]) + decryptor.finalize()
+	f = open("decrypt" + ext, 'w+')
+	f.write(str(output))
 
+test = MyFileEncrypt("test.txt")
+MyDecrypt(test[0], test[1], test[2], test[3])
+
+
+
+
+
+
+
+
+
+
+# def MyEncryptMAC(message, EncKey, HMACKey):
+# 	backend = default_backend()
+# 	IV = os.urandom(16)
+# 	cipher = Cipher(algorithms.AES(EncKey), modes.CBC(IV), backend=backend)
+# 	encryptor = cipher.encryptor()
+# 	buf = bytearray(len(message) + 127)
+# 	len_encrypted = encryptor.update_into(message, buf)
+# 	C = bytes(buf[:len_encrypted]) + encryptor.finalize()
+#
+# 	tag = hmac.HMAC(HMACKey, hashes.SHA256(), backend=default_backend())
+# 	tag.update(C)
+# 	tag.finalize()
+# 	return(C, IV, tag)
+#
+# def MyFileEncryptMAC(filepath):
+# 	EncKey = os.urandom(32)
+# 	HMACKey = os.urandom(32)
+# 	f = open(filepath, 'r')
+# 	ext = os.path.splitext(filepath)[1]
+# 	message = f.read()
+# 	message = bytes(message.encode('utf8'))
+# 	cipher = MyEncryptMAC(message, EncKey, HMACKey)
+# 	C = cipher[0]
+# 	IV = cipher[1]
+# 	tag = cipher[2]
+# 	return(C, IV, tag, EncKey, HMACKey, ext)
+#
+# def MyDecryptMAC(C, IV, tag, EncKey, HMACKey, ext):
+# 	backend = default_backend()
+# 	h = hmac.HMAC(HMACKey, hashes.SHA256(), backend=default_backend())
+# 	h.update(C)
+# 	h.verify(C)
+# 	h = h.finalize()
+#
+# 	cipher = Cipher(algorithms.AES(EncKey), modes.CBC(IV), backend=backend)
+# 	decryptor = cipher.decryptor()
+# 	buf = bytearray(len(C) + 127)
+# 	len_decrypted = decryptor.update_into(C, buf)
+# 	output = bytes(buf[:len_decrypted]) + decryptor.finalize()
+# 	f = open("decryptMAC" + ext, 'w+')
+# 	f.write(str(output))
+#
+# test = MyFileEncryptMAC("test.txt")
+# MyDecryptMAC(test[0], test[1], test[2], test[3], test[4], test[5])
